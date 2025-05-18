006e2a9e7055fbac1d5cd8afc8ac069ab8579f2d
Donovan Daniels <hewwo@yiff.rocks>
2025-05-18 17:59:32 -0500

    Add stats route

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { registerRoutes as registerPostViewsRoutes } from "./track/postViews.js"
 import { registerRoutes as registerSearchesRoutes } from "./track/searches.js";
 import { registerRoutes as registerMissedSearchesRoutes } from "./track/missedSearches.js";
 import { registerRoutes as registerApiKeyUsagesRoutes } from "./track/apiKeyUsages.js";
+import client from "./client.js";
 import Fastify from "fastify";
 import FastifyMiddleware from "@fastify/middie";
 import FastifyJWT from "@fastify/jwt";
@@ -50,6 +51,38 @@ registerPostViewsRoutes(app);
 registerSearchesRoutes(app);
 
 app.get("/up", async(request, reply) => reply.status(204).send());
+app.get("/stats", async(request, reply) => {
+    const viewCount = Number((await (await client.query({
+        query:  "SELECT COUNT(*) as count FROM post_views",
+        format: "JSON"
+    })).json<{ count: string; }>()).data[0].count);
+    const searchCount = Number((await (await client.query({
+        query:  "SELECT COUNT(*) as count FROM post_searches",
+        format: "JSON"
+    })).json<{ count: string; }>()).data[0].count);
+    const missedSearchCount = Number((await (await client.query({
+        query:  "SELECT COUNT(*) as count FROM missed_searches",
+        format: "JSON"
+    })).json<{ count: string; }>()).data[0].count);
+    const schemaVersion = Number((await (await client.query({
+        query:  "SELECT version FROM versions ORDER BY version DESC LIMIT 1",
+        format: "JSON"
+    })).json<{ version: string; }>()).data[0].version);
+    const clickhouseVersion = (await (await client.query({
+        query:  "SELECT value as version FROM system.build_options WHERE name = 'VERSION_DESCRIBE'",
+        format: "JSON"
+    })).json<{ version: string; }>()).data[0].version;
+    const ping = await client.ping();
+    return reply.status(200).send({
+        missedSearchCount,
+        searchCount,
+        viewCount,
+        schemaVersion,
+        clickhouseVersion,
+        healthy: ping.success,
+        error:   ping.success ? undefined : `${ping.error.name}: ${ping.error.message}`
+    });
+});
 
 await app.listen({
     host: "0.0.0.0",
